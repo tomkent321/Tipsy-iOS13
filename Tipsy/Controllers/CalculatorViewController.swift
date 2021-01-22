daf0f183aeed2cad5fe723c6a3ad56e7755a8d68
Tom K <tomkent321@yahoo.com>
2021-01-22 16:25:16 -0700

    added selection calculations in  CalculationViewController

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -11,20 +11,43 @@ import UIKit
 class CalculatorViewController: UIViewController {
 
     @IBOutlet weak var billTextField: UITextField!
-    
-    
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
+    var pctSelected = 0.1
+    var tipperNum = 2
+    
+   
     @IBAction func tipChanged(_ sender: UIButton) {
+        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        if sender.currentTitle == "0%"{
+            zeroPctButton.isSelected = true
+                pctSelected = 0.0
+            } else if sender.currentTitle == "10%"{
+                tenPctButton.isSelected = true
+                
+                pctSelected = 0.1
+            } else {
+                twentyPctButton.isSelected = true
+                zeroPctButton.isSelected = false
+                pctSelected = 0.2
+            }
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%0.0f",sender.value)
+        tipperNum = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: Any) {
+        print("tip: \(pctSelected)")
+        print("tippers: \(tipperNum)")
     }
     
     
--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -20,16 +20,8 @@ class ResultsViewController: UIViewController {
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
+        
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
